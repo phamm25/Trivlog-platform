a67e3e6a2e11e0505604981783cb70346a68a0c1
phamm25 <122081592+phamm25@users.noreply.github.com>
2023-01-23 10:58:08 -0500

    Update app.js

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,63 +1,3 @@
-// const fs = require("fs");
-// const path = require("path");
-
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const mongoose = require("mongoose");
-
-// const placesRoutes = require("./routes/places-routes");
-// const usersRoutes = require("./routes/users-routes");
-// const HttpError = require("./models/http-error");
-
-// const app = express();
-
-// app.use(bodyParser.json());
-
-// app.use("/uploads/images", express.static(path.join("uploads", "images")));
-
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//   );
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-
-//   next();
-// });
-
-// app.use("/api/places", placesRoutes);
-// app.use("/api/users", usersRoutes);
-
-// app.use((req, res, next) => {
-//   const error = new HttpError("Could not find this route.", 404);
-//   throw error;
-// });
-
-// app.use((error, req, res, next) => {
-//   if (req.file) {
-//     fs.unlink(req.file.path, (err) => {
-//       console.log(err);
-//     });
-//   }
-//   if (res.headerSent) {
-//     return next(error);
-//   }
-//   res.status(error.code || 500);
-//   res.json({ message: error.message || "An unknown error occurred!" });
-// });
-
-// mongoose
-//   .connect(
-//     `mongodb+srv://phamm:oycOnUGVffTjyPtd@cluster0.9wp5xit.mongodb.net/mern?retryWrites=true&w=majority`
-//   )
-//   .then(() => {
-//     app.listen(5000);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
 const fs = require('fs');
 const path = require('path');
 
@@ -109,7 +49,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    'mongodb+srv://phamm:YU9VKsljkJLiPmVw@cluster0.9wp5xit.mongodb.net/mern?retryWrites=true&w=majority'
+    'mongodb+srv://<user>:<password>@cluster0.9wp5xit.mongodb.net/mern?retryWrites=true&w=majority'
   )
   .then(() => {
     app.listen(process.env.PORT || 5000);
